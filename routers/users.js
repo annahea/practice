766f2fc25b930c91a674dc64edd22a1b79b95cc9
mohn <mohn@mohndeMacBook-Pro.local>
2019-11-03 23:36:55 +0800

    修改已存在邮箱但仍然可注册成功问题

--- routers/users.js
+++ routers/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const mysql = require('../config/db');
-const userModel = require('../models/user-password');
-const router = express.Router();
+const mysql = require('../config/db'); //配置文件
+const userModel = require('../models/user-password'); //数据库增删改查
+const router = express.Router(); //路由模块
 
 //注册页面路由
 router.get('/create', (req, res) => {
@@ -9,16 +9,37 @@ router.get('/create', (req, res) => {
 });
 
 //注册操作路由
+
+//存储到数据库
 router.post('/store', (req, res) => {
-    var user = [req.body.username, req.body.password, req.body.email];
-    mysql.query(userModel, user, function(err, result) {
-        if (err) {
-            console.log('[INSERT ERROR] - ', err.message);
-            return;
-        } else {
-            res.send("注册成功")
+    if (!req.body.username || !req.body.password || !req.body.email) {
+        res.send("请输入完整注册信息！");
+        return;
+    };
+    //存入数据
+
+    //判断数据是否已经存在
+    mysql.query(userModel.getUserByEmail, [req.body.email], function(err, result) {
+        if (err) { throw err } else {
+            for (var i = 0; i < result.length; i++) { //遍历结果
+                if (result[i].email == req.body.email) {
+                    res.send('用户已存在，请重新输入');
+                    return;
+                };
+            };
         };
+        //存入数据库
+        const user = [req.body.username, req.body.password, req.body.email];
+        mysql.query(userModel.insert, user, function(err, result) {
+            if (err) {
+                console.log('[INSERT ERROR] - ', err.message);
+                return;
+            } else {
+                res.send('注册成功');
+                return;
+            };
+        });
+
     });
 });
-
 module.exports = router;
