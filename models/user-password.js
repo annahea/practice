9b21c0d09df82d4628ac5155bc8c2f79976e9842
mohn <mohn@mohndeMacBook-Pro.local>
2019-11-03 23:37:34 +0800

    增删减查格式模块

--- models/user-password.js
+++ models/user-password.js
@@ -1,2 +1,8 @@
-const add = 'INSERT INTO user_password(id,username,password,email) VALUES(0,?,?,?)';
-module.exports = add;
+const userModel = {
+    insert: 'INSERT INTO user_password(id,username,password,email) VALUES(0,?,?,?)', // 插入数据
+    drop: 'DROP TABLE user_password', // 删除表中所有的数据
+    queryAll: 'SELECT * FROM user_password', // 查找表中所有数据
+    getUserById: 'SELECT * FROM user_password WHERE id =?', // 查找符合条件的数据
+    getUserByEmail: 'SELECT * FROM user_password WHERE email =?', //查找符合邮箱的数据
+};
+module.exports = userModel;
